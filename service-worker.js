2febb9c44936e12abced32e0e01774a7d7f02d8a
harungunay1334 <harungunay1334@gmail.com>
2026-03-31 20:02:48 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'saat-takip-v1';
-const ASSETS = [
+var CACHE_NAME = 'saat-takip-v2';
+var ASSETS = [
     './',
     './index.html',
     './style.css',
@@ -9,20 +9,46 @@ const ASSETS = [
     './icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+// Yeni versiyon yuklenince eski cache silinir
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(ASSETS);
-            })
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ASSETS);
+        })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+// Eski cache versiyonlarini temizle
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(name) {
+                    return name !== CACHE_NAME;
+                }).map(function(name) {
+                    return caches.delete(name);
+                })
+            );
+        }).then(function() {
+            return self.clients.claim();
+        })
+    );
+});
+
+// Network-first: Once internetten al, internet yoksa cache'den goster
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
+        fetch(event.request).then(function(response) {
+            // Basarili cevabi cache'e de kaydet
+            var responseClone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+                cache.put(event.request, responseClone);
+            });
+            return response;
+        }).catch(function() {
+            // Internet yoksa cache'den goster
+            return caches.match(event.request);
+        })
     );
 });
